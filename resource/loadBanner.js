3d5dca97138c05797d8ba7fa98b5928c7f1ee427
outlookxp <outlookxp@gmail.com>
2025-05-02 00:43:44 +0800

    更新：1. 添加中英文版本首页动态年份计算 2. 更新在线预约链接 3. 更新地址信息

--- resource/loadBanner.js
+++ resource/loadBanner.js
@@ -1,5 +1,10 @@
 function loadBanner() {
-    fetch('/public/banner.html')
+    // 获取当前页面的语言
+    const currentLang = document.documentElement.lang;
+    // 当lang属性为空或为中文时使用中文版，否则使用英文版
+    const bannerPath = (!currentLang || currentLang === 'zh') ? '/zh/public/banner.html' : '/en/public/banner.html';
+    
+    fetch(bannerPath)
         .then(response => response.text())
         .then(html => {
             document.body.insertAdjacentHTML('afterbegin', html);
--- resource/loadFooter.js
+++ resource/loadFooter.js
@@ -1,5 +1,10 @@
 function loadFooter() {
-    fetch('/public/footer.html')
+    // 获取当前页面的语言
+    const currentLang = document.documentElement.lang;
+    // 当lang属性为空或为中文时使用中文版，否则使用英文版
+    const footerPath = (!currentLang || currentLang === 'zh') ? '/zh/public/footer.html' : '/en/public/footer.html';
+    
+    fetch(footerPath)
         .then(response => response.text())
         .then(html => {
             document.body.insertAdjacentHTML('beforeend', html);
